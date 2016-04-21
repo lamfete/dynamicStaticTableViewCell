0c51aed16452406b904e125dbdc5c500c0715a04
Agust Lofianto <xiang@Agusts-Mac.local>
2016-04-21 22:27:56 +0700

    first & final commit

---
+++ dynamicStaticTableView/ProductTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductTableViewCell.swift
+//  dynamicStaticTableView
+//
+//  Created by Agust Lofianto on 4/20/16.
+//  Copyright © 2016 xiang. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
